dbf323633bba8e3a905c9f600a457c381be2c698
Jean-Marc Da Pozzo <jmdapozzo@macdap.com>
2022-12-20 10:54:09 -0500

    Maintenance: Improved and reshaped user management and access token

--- routes/management.js
+++ routes/management.js
@@ -3,45 +3,51 @@ const router = express.Router();
 const axios = require("axios");
 const checkJwtBackend = require("../auth/check-jwt-backend");
 
-var config = {
-  method: "POST",
-  url: "https://macdap.us.auth0.com/oauth/token",
-  headers: { "content-type": "application/x-www-form-urlencoded" },
-  data: new URLSearchParams({
-    grant_type: "client_credentials",
-    client_id: process.env.AUTH0_CLIENT_ID,
-    client_secret: process.env.AUTH0_CLIENT_SECRET,
-    audience: process.env.AUTH0_AUDIENCE_MANAGEMENT,
-  }),
-};
-
-var accessToken;
+let accessToken;
 
 const getAccessToken = async () => {
-  console.log("Getting Token");
   try {
-    const response = await axios.request(config);
-    expires_in = response.data.expires_in;
-    setTimeout(() => {accessToken = undefined; console.log("Clear accessToken")}, 10000);
-    console.log("expiration in ", expires_in);
-    return response.data.access_token;
+    if (accessToken === undefined) {
+      console.log("Getting a new access token");
+      const response = await axios.request({
+        method: "POST",
+        url: "https://macdap.us.auth0.com/oauth/token",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        data: new URLSearchParams({
+          grant_type: "client_credentials",
+          client_id: process.env.AUTH0_CLIENT_ID,
+          client_secret: process.env.AUTH0_CLIENT_SECRET,
+          audience: process.env.AUTH0_AUDIENCE_MANAGEMENT,
+        }),
+      });
+      expires_in = response.data.expires_in;
+      setTimeout(() => {
+        accessToken = undefined;
+        console.log("Access token expired");
+      }, expires_in * 1000);
+      accessToken = response.data.access_token;
+    }
+    return accessToken;
   } catch (error) {
     console.error(error);
   }
 };
 
-const getPublic = async (req, res, next) => {
+const getUsers = async (req, res, next) => {
   try {
-    if (accessToken === undefined) {
-        accessToken = await getAccessToken();
-        console.log("Getting a new access token");
-    }
-    res.send(accessToken);
+    var token = await getAccessToken();
+    const response = await axios.request({
+      method: "GET",
+      url: "https://macdap.us.auth0.com/api/v2/users",
+      headers: { authorization: `Bearer ${token}` },
+    });
+    res.send(response.data);
   } catch (error) {
-    res.status(400).json({ dbError: `db error - ${err.detail}` });
+    console.error(`getUser: ${error.name} ${error.code} - error ${error.response.status}, ${error.response.statusText}`);
+    next(error);
   }
 };
 
-router.get("/user", (req, res, next) => getPublic(req, res, next));
+router.get("/user/v2", checkJwtBackend, (req, res, next) => getUsers(req, res, next));
 
 module.exports = router;
